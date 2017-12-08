bbdd00f73a917f11a967a98684d75957805c21f2
brettmortensen <mortensen.brett@gmail.com>
2017-12-08 09:13:24 -0700

    added is_being_aggregated field

--- src/main/java/atrium/models/Member.java
+++ src/main/java/atrium/models/Member.java
@@ -8,6 +8,7 @@ public class Member {
     private String has_processed_transactions;
     private String identifier;
     private String institution_code;
+    private String is_being_aggregated;
     private String metadata;
     private String name;
     private String status;
@@ -42,6 +43,10 @@ public class Member {
         return institution_code;
     }
 
+    public String getIs_Being_Aggregated() {
+        return is_being_aggregated;
+    }
+
     public String getMetadata() {
         return metadata;
     }
@@ -72,6 +77,7 @@ public class Member {
         sb.append(", has_processed_transactions='").append(has_processed_transactions).append('\'');
         sb.append(", identifier='").append(identifier).append('\'');
         sb.append(", institution_code='").append(institution_code).append('\'');
+        sb.append(", is_being_aggregated='").append(is_being_aggregated).append('\'');
         sb.append(", metadata='").append(metadata).append('\'');
         sb.append(", name='").append(name).append('\'');
         sb.append(", status='").append(status).append('\'');
@@ -99,6 +105,7 @@ public class Member {
         if (identifier != null ? !identifier.equals(member.identifier) : member.identifier != null) return false;
         if (institution_code != null ? !institution_code.equals(member.institution_code) : member.institution_code != null)
             return false;
+        if (is_being_aggregated != null ? !is_being_aggregated.equals(member.is_being_aggregated) : member.is_being_aggregated != null) return false;
         if (metadata != null ? !metadata.equals(member.metadata) : member.metadata != null) return false;
         if (name != null ? !name.equals(member.name) : member.name != null) return false;
         if (status != null ? !status.equals(member.status) : member.status != null) return false;
@@ -116,6 +123,7 @@ public class Member {
         result = 31 * result + (has_processed_transactions != null ? has_processed_transactions.hashCode() : 0);
         result = 31 * result + (identifier != null ? identifier.hashCode() : 0);
         result = 31 * result + (institution_code != null ? institution_code.hashCode() : 0);
+        result = 31 * result + (is_being_aggregated != null ? is_being_aggregated.hashCode() : 0);
         result = 31 * result + (metadata != null ? metadata.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (status != null ? status.hashCode() : 0);

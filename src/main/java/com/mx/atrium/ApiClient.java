67933e95e1dae6f08533c7b1d9bf3aebc524d0c8
brettmortensen <11483419+brettmortensen@users.noreply.github.com>
2021-03-04 10:30:50 -0700

    Add last_payment_at
    Adds `last_payment_at` field to the Account model.

--- src/main/java/com/mx/atrium/ApiClient.java
+++ src/main/java/com/mx/atrium/ApiClient.java
@@ -81,7 +81,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Swagger-Codegen/2.10.4/java");
+        setUserAgent("Swagger-Codegen/2.10.5/java");
 
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
